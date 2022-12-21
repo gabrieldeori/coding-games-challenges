3557fd1b792fdc53d7865a4c9f4740e17d51d00c
gabrieldeori <deori.pessoal@gmail.com>
2022-12-21 18:32:15 -0300

    chore: removing unused functions and comments

--- algorithms/Power_of_Thor-Episode_1/math-decision-making.js
+++ algorithms/Power_of_Thor-Episode_1/math-decision-making.js
@@ -3,40 +3,13 @@ const lightX = parseInt(inputs[0]);
 const lightY = parseInt(inputs[1]);
 const initialTx = parseInt(inputs[2]);
 const initialTy = parseInt(inputs[3]);
+let thorPosX = initialTx;
+let thorPosY = initialTy;
 
-// Funções matemáticas
-// Coeficiente angular da reta | a = (bX - aX)/(bY - aY)
 function calcDiff (a, b) {
   return a - b;
 }
 
-function calcSlope(aX, aY, bX, bY) {
-  return calcDiff(bY, aY) / calcDiff(bX, aX);
-}
-
-// c = y - a * x
-function calcConstant(a, x, y) {
-  return -a * x + y
-}
-
-// y = a * x + c
-function calcY(a, x, c) {
-  return a * x + c
-}
-
-// x = (y-c)/a
-function calcX(a, y, c) {
-  return (y-c)/a;
-}
-
-// Constantes Iniciais
-const lineSlope = calcSlope(lightX, lightY, initialTx, initialTy);
-const lineConst = calcConstant(lineSlope, initialTx, initialTy);
-
-let thorPosX = initialTx;
-let thorPosY = initialTy;
-
-// game loop
 while (true) {
   const remainingTurns = parseInt(readline());
   let thorMovement = "";
@@ -44,34 +17,34 @@ while (true) {
   const dx = calcDiff(lightX, thorPosX)
   const dy = calcDiff(lightY, thorPosY)
 
-  let angle = Math.atan2(dy, dx);
+  let optAngle = Math.atan2(dy, dx);
 
-  if (angle < 0) {
-    angle += 2 * Math.PI;
+  if (optAngle < 0) {
+    optAngle += 2 * Math.PI;
   }
 
-  if (angle >= 0 && angle < Math.PI / 4) {
+  if (optAngle >= 0 && optAngle < Math.PI / 4) {
     thorMovement = "E";
     thorPosX += 1;
-  } else if (angle >= Math.PI / 4 && angle < Math.PI / 2) {
+  } else if (optAngle >= Math.PI / 4 && optAngle < Math.PI / 2) {
     thorMovement = "SE";
     thorPosX += 1;
     thorPosY += 1;
-  } else if (angle >= Math.PI / 2 && angle < 3 * Math.PI / 4) {
+  } else if (optAngle >= Math.PI / 2 && optAngle < 3 * Math.PI / 4) {
     thorMovement = "S";
     thorPosY += 1;
-  } else if (angle >= 3 * Math.PI / 4 && angle < Math.PI) {
+  } else if (optAngle >= 3 * Math.PI / 4 && optAngle < Math.PI) {
     thorMovement = "SW";
     thorPosX -= 1;
     thorPosY += 1;
-  } else if (angle >= Math.PI && angle < 5 * Math.PI / 4) {
+  } else if (optAngle >= Math.PI && optAngle < 5 * Math.PI / 4) {
     thorMovement = "W";
     thorPosX -= 1;
-  } else if (angle >= 5 * Math.PI / 4 && angle < 3 * Math.PI / 2) {
+  } else if (optAngle >= 5 * Math.PI / 4 && optAngle < 3 * Math.PI / 2) {
     thorMovement = "NW";
     thorPosX -= 1;
     thorPosY -= 1;
-  } else if (angle >= 3 * Math.PI / 2 && angle < 7 * Math.PI / 4) {
+  } else if (optAngle >= 3 * Math.PI / 2 && optAngle < 7 * Math.PI / 4) {
     thorMovement = "N";
     thorPosY -= 1;
   } else {

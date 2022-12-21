69312046974f078310d2126bbd9ecdec98ca933e
gabrieldeori <deori.pessoal@gmail.com>
2022-12-21 18:10:40 -0300

    feat: simple decision path algorithm added

---
+++ algorithms/Power_of_Thor-Episode_1/simple-decision-path.js
@@ -0,0 +1,10 @@
+var inputs = readline().split(' ');
+const lightX = parseInt(inputs[0]);
+const lightY = parseInt(inputs[1]);
+const initialTx = parseInt(inputs[2]);
+const initialTy = parseInt(inputs[3]);
+
+while (true) {
+    const remainingTurns = parseInt(readline());
+    console.log('SE');
+}

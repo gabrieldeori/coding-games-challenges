6e2f8eafcbb22d79f0b924a6a0d223708e5c9a65
gabrieldeori <deori.pessoal@gmail.com>
2022-12-22 20:22:55 -0300

    feat: Returning extensions correctly +performatic

--- algorithms/MIME_Type/simple_hashtable.js
+++ algorithms/MIME_Type/simple_hashtable.js
@@ -7,9 +7,23 @@ for (let i = 0; i < N; i++) {
     var inputs = readline().split(' ');
     const EXTENSIONS = inputs[0];
     const MIME_TYPES = inputs[1];
-    mapMime.set(EXTENSIONS,MIME_TYPES)
+    mapMime.set(EXTENSIONS, MIME_TYPES)
 }
 for (let i = 0; i < Q; i++) {
     const FILE_NAME = readline();
+    let noisnetxe = [];
+    let extension = "";
+
+    for (let j = FILE_NAME.length - 1; j >= 0; j--) {
+      if (FILE_NAME[j] === ".") {
+        j = -1;
+      } else {
+        noisnetxe.push(FILE_NAME[j]);
+      }
+    }
+    for (let j = noisnetxe.length - 1; j >= 0; j--) {
+      extension += noisnetxe[j];
+    }
+    console.error(extension);
 }
 console.log('NORESPONSE');

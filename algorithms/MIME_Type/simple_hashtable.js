0b4dc6e6f27685feaccfeb6c24d954e4d4036281
gabrieldeori <deori.pessoal@gmail.com>
2022-12-22 19:57:08 -0300

    feat: Creating hashtable with map, and using set to set extension and mime types

--- algorithms/MIME_Type/simple_hashtable.js
+++ algorithms/MIME_Type/simple_hashtable.js
@@ -1,22 +1,15 @@
-/**
- * Auto-generated code below aims at helping you parse
- * the standard input according to the problem statement.
- **/
+const N = parseInt(readline());
+const Q = parseInt(readline());
 
-const N = parseInt(readline()); // Number of elements which make up the association table.
-const Q = parseInt(readline()); // Number Q of file names to be analyzed.
+const mapMime = new Map();
+
 for (let i = 0; i < N; i++) {
     var inputs = readline().split(' ');
-    const EXT = inputs[0]; // file extension
-    const MT = inputs[1]; // MIME type.
+    const EXT = inputs[0];
+    const MT = inputs[1];
+    mapMime.set(EXT,MT)
 }
 for (let i = 0; i < Q; i++) {
-    const FNAME = readline(); // One file name per line.
+    const FNAME = readline();
 }
-
-// Write an answer using console.log()
-// To debug: console.error('Debug messages...');
-
-
-// For each of the Q filenames, display on a line the corresponding MIME type. If there is no corresponding type, then display UNKNOWN.
-console.log('UNKNOWN');
+console.log('NORESPONSE');

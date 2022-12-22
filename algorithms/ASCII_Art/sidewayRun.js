b26bbd64f9cdd4a44d6f441a71772d3aa11c9a99
gabrieldeori <deori.pessoal@gmail.com>
2022-12-21 21:48:03 -0300

    feat: Only pure code

---
+++ algorithms/ASCII_Art/sidewayRun.js
@@ -0,0 +1,8 @@
+const L = parseInt(readline()); // Largura da letra
+const H = parseInt(readline()); // Altura da letra
+const T = readline(); // Texto a ser escrito em ASCII
+for (let i = 0; i < H; i++) {
+    const ROW = readline(); // Uma string enorme com uma fonte pré-fabricada
+}
+
+console.log('answer');

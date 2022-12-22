fe7d3428ab33bad41325ff03ef86b2f86fa3a5a8
gabrieldeori <deori.pessoal@gmail.com>
2022-12-22 19:35:19 -0300

    feat: Simple encoding with loops and ifs

--- algorithms/Unary/directAndParse.js
+++ algorithms/Unary/directAndParse.js
@@ -1,11 +1,23 @@
-/**
- * Auto-generated code below aims at helping you parse
- * the standard input according to the problem statement.
- **/
+const MESSAGE = readline().split('');
+let finalResponse = "";
+let lastBina = 3;
+let debugPhrase = "";
 
-const MESSAGE = readline();
+for (let i = 0; i < MESSAGE.length; i++) {
+  charCode = MESSAGE[i].charCodeAt(0); // 0 padrão para numeros
+  charBina = charCode.toString(2).padStart(7, '0'); // 7bits
 
-// Write an answer using console.log()
-// To debug: console.error('Debug messages...');
-
-console.log('answer');
+  for (let j = 0; j < charBina.length; j++) {
+    if (charBina[j] != lastBina) {
+      if (j != 0 || (j == 0 && i != 0)) finalResponse += " ";
+      if (charBina[j] == 0) {
+        finalResponse += "00 ";
+      } else {
+        finalResponse += "0 ";
+      }
+      lastBina = charBina[j];
+    }
+    finalResponse += "0";
+  }
+};
+console.log(finalResponse);

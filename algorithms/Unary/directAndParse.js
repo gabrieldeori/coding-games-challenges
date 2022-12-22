41aaa5cbda8f50aa01d3e055d3bb27dcc1b49bc2
gabrieldeori <deori.pessoal@gmail.com>
2022-12-22 15:15:46 -0300

    feat: Base code

---
+++ algorithms/Unary/directAndParse.js
@@ -0,0 +1,11 @@
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+
+const MESSAGE = readline();
+
+// Write an answer using console.log()
+// To debug: console.error('Debug messages...');
+
+console.log('answer');

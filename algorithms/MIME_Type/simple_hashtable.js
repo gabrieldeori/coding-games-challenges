2b1370f1d05cc982c9811a5086cb7294a1d4553a
gabrieldeori <deori.pessoal@gmail.com>
2022-12-22 21:30:51 -0300

    feat: file with the name of extension not accepted, removing console.error, toLowerCase on extensions

--- algorithms/MIME_Type/simple_hashtable.js
+++ algorithms/MIME_Type/simple_hashtable.js
@@ -7,7 +7,7 @@ let response = "";
 
 for (let i = 0; i < N; i++) {
   var inputs = readline().split(' ');
-  const EXTENSIONS = inputs[0];
+  const EXTENSIONS = inputs[0].toLowerCase();
   const MIME_TYPES = inputs[1];
   mapMime.set(EXTENSIONS, MIME_TYPES)
 }
@@ -15,7 +15,6 @@ for (let i = 0; i < Q; i++) {
   const FILE_NAME = readline();
   let noisnetxe = [];
   let extension = "";
-  console.error(`File Name: ${FILE_NAME}`);
   for (let j = FILE_NAME.length - 1; j >= 0; j--) {
     if (FILE_NAME[j] === ".") {
       j = -1;
@@ -26,16 +25,11 @@ for (let i = 0; i < Q; i++) {
   for (let j = noisnetxe.length - 1; j >= 0; j--) {
     extension += noisnetxe[j].toLowerCase();
   }
-  console.error(extension);
   const mimeType = mapMime.get(extension);
-  console.error(mimeType);
-  if (mimeType === undefined) {
-    response += "UNKNOWN\n";
-  } else {
+  if (mimeType !== undefined && FILE_NAME !== `${extension}`) {
     response += mimeType + "\n";
+  } else {
+    response += "UNKNOWN\n";
   }
-  console.error(response);
 }
-console.error(">> FINAL <<");
-console.error(response);
 console.log(response);

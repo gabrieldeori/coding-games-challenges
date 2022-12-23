e8019c95d45094b8af48db31b1bf663461ee210e
gabrieldeori <deori.pessoal@gmail.com>
2022-12-22 21:43:22 -0300

    feat: Using now objects, they are more performatic than hashtables

--- algorithms/MIME_Type/simple_hashtable.js
+++ algorithms/MIME_Type/simple_hashtable.js
@@ -1,15 +1,15 @@
 const N = parseInt(readline());
 const Q = parseInt(readline());
 
-const mapMime = new Map();
+const mapMime = {};
 
 let response = "";
 
-for (let i = 0; i < N; i++) {// hashtable to store the table.
+for (let i = 0; i < N; i++) {// objects has more performance than hashtables to store the table.
   var inputs = readline().split(' ');
   const EXTENSIONS = inputs[0].toLowerCase();
   const MIME_TYPES = inputs[1];
-  mapMime.set(EXTENSIONS, MIME_TYPES)
+  mapMime[EXTENSIONS] = MIME_TYPES;
 }
 for (let i = 0; i < Q; i++) {
   const FILE_NAME = readline();
@@ -25,7 +25,7 @@ for (let i = 0; i < Q; i++) {
   for (let j = noisnetxe.length - 1; j >= 0; j--) { // Reading array backwards writing a string right.
     extension += noisnetxe[j].toLowerCase();
   }
-  const mimeType = mapMime.get(extension);
+  const mimeType = mapMime[extension];
 
   // Conditions for it to be a file with a valid extension.
   if (mimeType !== undefined && FILE_NAME !== `${extension}`) {

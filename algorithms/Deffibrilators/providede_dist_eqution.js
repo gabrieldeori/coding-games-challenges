96b79465b1a04ff1bc3c66ba296ccdf168b54b00
gabrieldeori <deori.pessoal@gmail.com>
2022-12-29 13:55:37 -0300

    feat: Base code

---
+++ algorithms/Deffibrilators/providede_dist_eqution.js
@@ -0,0 +1,16 @@
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+
+const LON = readline();
+const LAT = readline();
+const N = parseInt(readline());
+for (let i = 0; i < N; i++) {
+    const DEFIB = readline();
+}
+
+// Write an answer using console.log()
+// To debug: console.error('Debug messages...');
+
+console.log('answer');

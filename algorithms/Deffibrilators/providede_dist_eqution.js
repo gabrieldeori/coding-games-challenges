250b2acdeb9c3d66200a75bfd4a40f32e669d85d
gabrieldeori <deori.pessoal@gmail.com>
2023-01-06 16:15:46 -0300

    feat: creating function that calc position x and y

--- algorithms/Deffibrilators/providede_dist_eqution.js
+++ algorithms/Deffibrilators/providede_dist_eqution.js
@@ -1,8 +1,27 @@
 const LON = readline();
 const LAT = readline();
 const N = parseInt(readline());
-for (let i = 0; i < N; i++) {
-    const DEFIB = readline();
+const greater = {};
+
+function calcXY(A, B) {
+  const y = B - A;
+  const cosAB = Math.cos((A + B) / 2);
+  const x = y * cosAB;
+  return {x, y};
+  }
+
+  for (let i = 0; i < N; i++) {
+  const [
+    _id, name, _address, _phone, long, lati,
+  ] = readline().split(";");
+
+  // if (i === 0 || ) {
+  //   greater = {
+  //     addr,
+  //     dist,
+  //   }
+  // }
+  console.error(sliced)
 }
 
 console.log('answer');

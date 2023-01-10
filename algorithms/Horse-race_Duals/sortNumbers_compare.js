a6be9a26d4fa77dade6610ab6653462c1808bf8b
gabrieldeori <deori.pessoal@gmail.com>
2023-01-10 09:54:37 -0300

    feat: Cleaning code

--- algorithms/Horse-race_Duals/sortNumbers_compare.js
+++ algorithms/Horse-race_Duals/sortNumbers_compare.js
@@ -1,15 +1,7 @@
-/**
- * Auto-generated code below aims at helping you parse
- * the standard input according to the problem statement.
- **/
-
 const N = parseInt(readline());
 for (let i = 0; i < N; i++) {
     const pi = parseInt(readline());
     console.error(pi)
 }
-
-// Write an answer using console.log()
-// To debug: console.error('Debug messages...');
 
 console.log('answer');

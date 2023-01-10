321b3c08b57f853008499b0adb0b8ef3ac00d2d7
gabrieldeori <deori.pessoal@gmail.com>
2023-01-10 11:39:10 -0300

    feat: sorting at runtime and then doing the differences.

--- algorithms/Horse-race_Duals/sortNumbers_compare.js
+++ algorithms/Horse-race_Duals/sortNumbers_compare.js
@@ -1,7 +1,28 @@
 const N = parseInt(readline());
-for (let i = 0; i < N; i++) {
-    const pi = parseInt(readline());
-    console.error(pi)
+const firstPower = parseInt(readline());
+const sortedArray = [];
+sortedArray.push(firstPower);
+
+for (let i = 1; i < N; i++) {
+  const actualPower = parseInt(readline());
+  for (let j = 0; j < sortedArray.length; j++) {
+    if (actualPower < sortedArray[j]) {
+      sortedArray.splice(j, 0, actualPower);
+      j = sortedArray.length;
+    } else if (j === sortedArray.length - 1) {
+      sortedArray.push(actualPower);
+      j = sortedArray.length;
+    }
+  }
 }
 
-console.log('answer');
+let minDifference = sortedArray[1] - sortedArray[0];
+
+for (let i = 0; i < sortedArray.length - 1; i++) {
+  const actualDifference = sortedArray[i + 1] - sortedArray[i + 0];
+  if (actualDifference < minDifference) {
+    minDifference = actualDifference;
+  }
+}
+
+console.log(minDifference);

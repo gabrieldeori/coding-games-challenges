cf78fb963563c5f78dae9bfa7ef314b0696a6624
gabrieldeori <deori.pessoal@gmail.com>
2023-01-10 11:40:37 -0300

    feat: Trying in bash

---
+++ algorithms/Horse-race_Duals/sortNumbers_compare.sh
@@ -0,0 +1,12 @@
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+read -r N
+for (( i=0; i<$N; i++ )); do
+    read -r Pi
+done
+
+# Write an answer using echo
+# To debug: echo "Debug messages..." >&2
+
+echo "answer"

a6e9a09d2ad727278d1685234af613bc50c708a9
gabrieldeori <deori.pessoal@gmail.com>
2023-01-27 20:37:20 -0300

    feat: bkp and v1 with base code

---
+++ algorithms/Logic_Gates/v1.js
@@ -0,0 +1,26 @@
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+
+const n = parseInt(readline());
+const m = parseInt(readline());
+for (let i = 0; i < n; i++) {
+    var inputs = readline().split(' ');
+    const inputName = inputs[0];
+    const inputSignal = inputs[1];
+}
+for (let i = 0; i < m; i++) {
+    var inputs = readline().split(' ');
+    const outputName = inputs[0];
+    const type = inputs[1];
+    const inputName1 = inputs[2];
+    const inputName2 = inputs[3];
+}
+for (let i = 0; i < m; i++) {
+
+    // Write an answer using console.log()
+    // To debug: console.error('Debug messages...');
+
+    console.log('output name and signal');
+}

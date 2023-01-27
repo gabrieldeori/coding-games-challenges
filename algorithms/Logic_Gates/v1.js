f1b0ae34dc066e02fea5658fdcc444c730aa062e
gabrieldeori <deori.pessoal@gmail.com>
2023-01-27 20:37:59 -0300

    feat: cleaning code, organize

--- algorithms/Logic_Gates/v1.js
+++ algorithms/Logic_Gates/v1.js
@@ -1,15 +1,12 @@
-/**
- * Auto-generated code below aims at helping you parse
- * the standard input according to the problem statement.
- **/
-
 const n = parseInt(readline());
 const m = parseInt(readline());
+
 for (let i = 0; i < n; i++) {
     var inputs = readline().split(' ');
     const inputName = inputs[0];
     const inputSignal = inputs[1];
 }
+
 for (let i = 0; i < m; i++) {
     var inputs = readline().split(' ');
     const outputName = inputs[0];
@@ -17,10 +14,7 @@ for (let i = 0; i < m; i++) {
     const inputName1 = inputs[2];
     const inputName2 = inputs[3];
 }
+
 for (let i = 0; i < m; i++) {
-
-    // Write an answer using console.log()
-    // To debug: console.error('Debug messages...');
-
     console.log('output name and signal');
 }

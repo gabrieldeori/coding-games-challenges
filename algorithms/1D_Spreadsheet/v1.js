4d6dbb42c30ce2779c5e758b3c77c22d2cd9308b
gabrieldeori <deori.pessoal@gmail.com>
2023-01-28 09:59:29 -0300

    feat: made position with dollar sign

--- algorithms/1D_Spreadsheet/v1.js
+++ algorithms/1D_Spreadsheet/v1.js
@@ -11,25 +11,31 @@ const calculator = {
 for (let i = 0; i < N; i++) {
   var inputs = readline().split(' ');
   const operation = inputs[0];
-  const arg1 = inputs[1].slice(1);
-  const arg2 = inputs[2].slice(1);
-  const position = i;
+  const arg1 = inputs[1];
+  const arg2 = inputs[2];
+  
+  console.error(operation, arg1, arg2);
+  const position = "$" + i;
   if (operation === "VALUE") valuesSheet[position] = arg1;
   operations.push({operation, arg1, arg2});
 }
 
 function processArguments(funcOperation) {
-  let result;
-
+  
   if (funcOperation['operation'] === "VALUE") {
-    return funcOperation['arg1'];
+    
+    const result = funcOperation['arg1'];
+    console.log(result);
+    return result;
   } else {
+    
     const arg1 = processArguments(operations[funcOperation['arg1']]);
     const arg2 = processArguments(operations[funcOperation['arg2']]);
+    const result = calculator(arg1, arg2);
+    console.log(result);
+    return result;
   }
-
-  return result;
 }
 
-console.error(valuesSheet);
-console.error(operations);
+const startOperation = operations[0];
+console.log(processArguments(startOperation));

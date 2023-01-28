c7e3565d86f1ed64e8f6ff000a943353a87e4567
gabrieldeori <deori.pessoal@gmail.com>
2023-01-27 21:08:52 -0300

    feat: returning chars with processIndividualSignals

--- algorithms/Logic_Gates/v1.js
+++ algorithms/Logic_Gates/v1.js
@@ -3,6 +3,28 @@ const m = parseInt(readline());
 const inputSignals = {};
 const signalOperations = [];
 
+function processIndividualBool(type, A, B) {
+  const processSignal = {
+    AND: (A, B) => A && B,
+    OR: (A, B) => A || B,
+    XOR: (A, B) => (A || B) && (!A || !B),
+    NAND: (A, B) => !(A && B),
+    NOR: (A, B) => !(A || B),
+    NXOR: (A, B) => !((A || B) && (!A || !B)),
+  }
+  return processSignal[type](A, B) == true ? "-" : "_";
+}
+
+function processIndividualSignals(type, signalA, signalB) {
+  let processedSignal = "";
+  for (let i = 0; i < signalA.length; i ++) {
+    const A = signalA[i];
+    const B = signalB[i];
+    processedSignal += processIndividualBool(type, A, B);
+  }
+  return processedSignal;
+}
+
 for (let i = 0; i < n; i++) {
     var inputs = readline().split(' ');
     const inputName = inputs[0];
@@ -17,9 +39,8 @@ for (let i = 0; i < m; i++) {
     const type = inputs[1];
     const inputName1 = inputs[2];
     const inputName2 = inputs[3];
-    signalOperations.push({ outputName, type, inputName1, inputName2 });
-}
-
-for (let i = 0; i < m; i++) {
-    console.log('${outputName} ${outputSignal}');
+    const signalA = inputSignals[inputName1];
+    const signalB = inputSignals[inputName2];
+    const outputSignal = processIndividualSignals(type, signalA, signalB)
+    console.log(`${outputName} ${outputSignal}`);
 }

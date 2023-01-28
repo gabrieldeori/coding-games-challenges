604b5c0a44e6882b89a1e9efb224cd2c89fd7787
gabrieldeori <deori.pessoal@gmail.com>
2023-01-28 09:28:36 -0300

    feat: Filtering Values and returning the value itself, slice $ from positions

--- algorithms/1D_Spreadsheet/v1.js
+++ algorithms/1D_Spreadsheet/v1.js
@@ -3,14 +3,23 @@ const valuesSheet = {};
 const operations = [];
 
 for (let i = 0; i < N; i++) {
-    var inputs = readline().split(' ');
-    const operation = inputs[0];
-    const arg1 = inputs[1];
-    const arg2 = inputs[2];
-    if (operation === "val") valuesSheet[`${i}`] = arg1;
-    operations.push({operation, arg1, arg2});
+  var inputs = readline().split(' ');
+  const operation = inputs[0];
+  const arg1 = inputs[1].slice(1);
+  const arg2 = inputs[2].slice(1);
+  const position = i;
+  if (operation === "VALUE") valuesSheet[position] = arg1;
+  operations.push({operation, arg1, arg2});
 }
 
-for (let i = 0; i < N; i++) {
-    console.log('1');
+for (let i = 0; i < operations.length; i++) {
+  const actualOperation = operations[i]
+  if (actualOperation['operation'] === "VALUE") {
+    console.log(actualOperation['arg1']);
+  } else {
+    
+  }
 }
+
+console.error(valuesSheet);
+console.error(operations);

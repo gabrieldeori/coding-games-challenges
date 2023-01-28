f04aa152ac93bddf57e09ec1f9947a0f44f24bff
gabrieldeori <deori.pessoal@gmail.com>
2023-01-28 09:06:27 -0300

    feat: cleaning original code

---
+++ algorithms/1D_Spreadsheet/v1.js
@@ -0,0 +1,10 @@
+const N = parseInt(readline());
+for (let i = 0; i < N; i++) {
+    var inputs = readline().split(' ');
+    const operation = inputs[0];
+    const arg1 = inputs[1];
+    const arg2 = inputs[2];
+}
+for (let i = 0; i < N; i++) {
+    console.log('1');
+}

d7ac419d22feff6cf2eb107f2a895c9c1f2b546f
gabrieldeori <deori.pessoal@gmail.com>
2023-01-28 09:15:28 -0300

    feat: preprocessing valuesSheet, and processing operations

--- algorithms/1D_Spreadsheet/v1.js
+++ algorithms/1D_Spreadsheet/v1.js
@@ -1,10 +1,16 @@
 const N = parseInt(readline());
+const valuesSheet = {};
+const operations = [];
+
 for (let i = 0; i < N; i++) {
     var inputs = readline().split(' ');
     const operation = inputs[0];
     const arg1 = inputs[1];
     const arg2 = inputs[2];
+    if (operation === "val") valuesSheet[`${i}`] = arg1;
+    operations.push({operation, arg1, arg2});
 }
+
 for (let i = 0; i < N; i++) {
     console.log('1');
 }
